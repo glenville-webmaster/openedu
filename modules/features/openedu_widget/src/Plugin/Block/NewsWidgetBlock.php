101da01f75be6641606bde82960b95ac12137a61
Josh Chambers <josh.chambers@glenville.edu>
2021-07-23 10:25:49 -0400

    Extracting Events Content type from Search and Widget

--- modules/features/openedu_widget/src/Plugin/Block/NewsWidgetBlock.php
+++ modules/features/openedu_widget/src/Plugin/Block/NewsWidgetBlock.php
@@ -10,14 +10,14 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
- * Class NewsAndEventsWidgetBlock.
+ * Class NewsWidgetBlock.
  *
  * @Block(
  *   id = "news_events_widget",
  *   admin_label = @Translation("News & Events Widget"),
  * )
  */
-class NewsAndEventsWidgetBlock extends BlockBase {
+class NewsWidgetBlock extends BlockBase {
 
   /**
    * {@inheritdoc}
@@ -45,14 +45,10 @@ class NewsAndEventsWidgetBlock extends BlockBase {
     $news_categories = \Drupal::entityTypeManager()
       ->getStorage('taxonomy_term')
       ->loadTree('news_categories');
-    $event_categories = \Drupal::entityTypeManager()
-      ->getStorage('taxonomy_term')
-      ->loadTree('event_categories');
 
     // Default form values.
     $widget_content_type_display = $config['widget_content_type_display'];
     $widget_content_news_categories = $config['widget_content_news_categories'];
-    $widget_content_event_categories = $config['widget_content_event_categories'];
 
     $form['widget_content_type_display'] = [
       '#type' => 'select',
@@ -60,9 +56,7 @@ class NewsAndEventsWidgetBlock extends BlockBase {
       '#default_value' => !empty($widget_content_type_display) ? $widget_content_type_display : '',
       '#required' => TRUE,
       '#options' => [
-        'news_events' => t('News & Events'),
         'news' => t('News'),
-        'events' => t('Events'),
       ],
     ];
 
@@ -80,20 +74,6 @@ class NewsAndEventsWidgetBlock extends BlockBase {
       ];
     }
 
-    if (count($event_categories)) {
-      $options = [];
-      foreach ($event_categories as $category) {
-        $options[$category->tid] = $category->name;
-      }
-      $form['widget_content_event_categories'] = [
-        '#type' => 'select',
-        '#default_value' => !empty($widget_content_event_categories) ? $widget_content_event_categories : '',
-        '#multiple' => TRUE,
-        '#title' => t('Event Categories'),
-        '#options' => $options,
-      ];
-    }
-
     return $form;
   }
 
@@ -103,7 +83,6 @@ class NewsAndEventsWidgetBlock extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['widget_content_type_display'] = $form_state->getValue('widget_content_type_display');
     $this->configuration['widget_content_news_categories'] = $form_state->getValue('widget_content_news_categories');
-    $this->configuration['widget_content_event_categories'] = $form_state->getValue('widget_content_event_categories');
   }
 
   /**
@@ -114,14 +93,8 @@ class NewsAndEventsWidgetBlock extends BlockBase {
     $config = $this->getConfiguration();
     switch ($config['widget_content_type_display']) {
 
-      case 'news':
-        return t('News');
-
-      case 'events':
-        return t('Events');
-
       default:
-        return t('News and Events');
+        return t('News');
 
     }
   }
@@ -134,25 +107,11 @@ class NewsAndEventsWidgetBlock extends BlockBase {
     $config = $this->getConfiguration();
     $widget_content_type_display = $config['widget_content_type_display'];
     $widget_content_news_categories = $config['widget_content_news_categories'];
-    $widget_content_event_categories = $config['widget_content_event_categories'];
 
     switch ($widget_content_type_display) {
-      case 'events':
-        $filters = ['field_events_category' => $widget_content_event_categories];
-        $rendered = $this->getNewsEventsView('events_widget_block', $filters);
-        break;
-
-      case 'news':
+      default:
         $filters = ['field_news_categories' => $widget_content_news_categories];
         $rendered = $this->getNewsEventsView('news_widget_block', $filters);
-        break;
-
-      default:
-        $filters = [
-          'field_events_category' => $widget_content_event_categories,
-          'field_news_categories' => $widget_content_news_categories,
-        ];
-        $rendered = $this->getNewsEventsView('news_events_widget_block', $filters);
         break;
     }
 
@@ -183,7 +142,7 @@ class NewsAndEventsWidgetBlock extends BlockBase {
     $view->execute();
 
     $renderable = [
-      '#theme' => 'openedu_news_events_widget',
+      '#theme' => 'openedu_news_widget',
       '#block_type' => $block_type,
       '#results' => $view->render(),
     ];
